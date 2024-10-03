3bcff488b8033519e9cc52f3d33666788de7e8cb
omarpf86 <omarpf86@gmail.com>
2024-10-03 13:32:20 -0300

    Update ticket.model.js

--- Proyecto_Final_OK_Perez_Fortunato_Omar/src/persistence/daos/mongodb/models/ticket.model.js
+++ Proyecto_Final_OK_Perez_Fortunato_Omar/src/persistence/daos/mongodb/models/ticket.model.js
@@ -6,9 +6,26 @@ export const tiketCollectionName = "ticket";
 const ticketSchema = new mongoose.Schema({
     code: { type: String, required: true },
     purchase_datetime: { type: String, required: true },
+    list: [
+        {
+            name: {
+            type: String,
+            required: true,  
+        }, 
+            selectedQuantity: {
+                type: Number,
+                required: true,
+            },
+            price: {
+                type: Number,
+                required: true,  
+            },
+         _id: false  
+        }
+    ],
     amount: { type: Number, required: true },
     purchaser: { type: String, required: true },
-});
+} );
 
 export const TicketModel = mongoose.model(
     tiketCollectionName,
