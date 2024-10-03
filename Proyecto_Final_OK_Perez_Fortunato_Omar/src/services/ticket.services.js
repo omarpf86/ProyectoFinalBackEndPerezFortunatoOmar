5b075205b2ba3f91377563d23509abe56a39b9a2
omarpf86 <omarpf86@gmail.com>
2024-10-03 13:29:28 -0300

    Update ticket.services.js

--- Proyecto_Final_OK_Perez_Fortunato_Omar/src/services/ticket.services.js
+++ Proyecto_Final_OK_Perez_Fortunato_Omar/src/services/ticket.services.js
@@ -1,5 +1,3 @@
-
-
 import * as cartService from "./cart.services.js";
 import * as productService from "./product.services.js";
 
@@ -14,29 +12,60 @@ const ticketDao= new TicketDaoMongo()
             if (!cart) return null;
 
             let amountAcc = 0;
+            let purcharsedProducts = [] 
+            let stockIssues = [];    
             if (cart.products.length > 0) {
                 for (const x of cart.products) {
                     const idProd = x.product._id;//tuve que agregaar el id porq el cart tien el populate
                     const prodDB = await productService.getById(idProd);
-                
 
+                    
+
+                    if (x.quantity > prodDB.stock) {
+                        stockIssues.push({
+                            product: prodDB.name,
+                            availableStock: prodDB.stock,
+                            requestedQuantity: x.quantity
+                        });
+                        continue; 
+                    }
+                    console.log ("insufucuentestock",stockIssues)
                     if (x.quantity <= prodDB.stock) {
+                        let name = prodDB.name
+                        let price = prodDB.price
+                        let selectedQuantity= x.quantity
+                        purcharsedProducts.push({ name,selectedQuantity, price })
                         const amount = x.quantity * prodDB.price;
-                        amountAcc += amount;
+                        let newStock = (prodDB.stock - x.quantity)
+                        console.log("en ticket service el newstock es", newStock)
+                        const updatedProduct = await productService.updateStock(idProd, { stock: newStock }) 
+                    
+                        
+                       amountAcc += amount;
                     } else return null;
                 }
             }
+            console.log("en ticket service purchasedproducts es", purcharsedProducts)
+            
+            if (stockIssues.length > 0) {
+                    return  {
+                        message: "Algunos productos no tienen suficiente stock.",
+                        stockIssues
+                    };
+            }
+            else {
+                const ticket = await ticketDao.create({
+                    code: `${Math.floor(Math.random() * 1000)}`,
+                    purchase_datetime: new Date().toLocaleString(),
+                    list: purcharsedProducts,
+                    amount: amountAcc,
+                    purchaser: user.email,
+                });
 
-            const ticket = await ticketDao.create({
-                code: `${Math.floor(Math.random() * 1000)}`,
-                purchase_datetime: new Date().toLocaleString(),
-                amount: amountAcc,
-                purchaser: user.email,
-            });
+                await cartService.clearCart(user.cid);
 
-            await cartService.clearCart(user.cid);
-
-            return ticket;
+                return ticket;
+            }    
 
         } catch (error) {
             throw new Error(error);
